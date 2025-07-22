7988bc253c01f6b087a4156f40bbac2dbbf3407d
Levente <belveder21@gmail.com>
2025-07-22 21:30:34 +0100

    New changes 1. added CPU count, 2. Added Proccess ID, 3 Added, Current user name.

--- something.py
+++ something.py
@@ -6,6 +6,9 @@ x = range(0, 100)
 for i in x:
     if i % 3:
         print(i)
-        
+
 print()
 print(f'CPU count: {os.cpu_count()}')
+print(os.get_terminal_size())
+print(f'Proccess ID: {os.getpid()}')
+print(f'Current User: {os.getlogin()}')
